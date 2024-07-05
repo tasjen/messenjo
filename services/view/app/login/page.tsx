ebcf93fe1bea79fa7350e4607aa4a826b6a29b7e
tasjen <129994991+tasjen@users.noreply.github.com>
2024-07-05 23:56:21 +0700

    Improve svg icons and sign in buttons

--- services/view/app/login/page.tsx
+++ services/view/app/login/page.tsx
@@ -1,11 +1,24 @@
-import GoogleLoginButton from "@/components/login/google-login-button";
-import GitHubLoginButton from "@/components/login/github-login-button";
+import { Button } from "@/components/ui/button";
+import { Icons } from "@/components/ui/icon";
+
+// use <a> instead of <Link> for sign in button to avoid making
+// unknown failed fetch request in the network tab of dev tools
 
 export default function LoginPage() {
   return (
     <main className="flex flex-col h-screen justify-center items-center gap-4">
-      <GoogleLoginButton />
-      <GitHubLoginButton />
+      <a href="/api/auth/login/google">
+        <Button>
+          <Icons.google className="inline w-4 h-4 mr-2" />
+          <span>Continue with Google</span>
+        </Button>
+      </a>
+      <a href="/api/auth/login/github">
+        <Button>
+          <Icons.gitHub className="inline w-4 h-4 mr-2" />
+          <span>Continue with GitHub</span>
+        </Button>
+      </a>
       <a href="/">
         <button>Home</button>
       </a>
